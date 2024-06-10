83c9f2daf90536774780b275171342ca2ef34f63
anoushkagarg003 <108232467+anoushkagarg003@users.noreply.github.com>
2024-06-10 15:48:46 +0530

    can find similarity scoring for one song at a time, against an entire df

--- recsystem.py
+++ recsystem.py
@@ -15,3 +15,20 @@ connection.close()
 # Display the DataFrame
 
 
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+# The song vector you want to compare
+def similarity_scoring(song_vector, df):
+    similarity_scores = cosine_similarity(df, song_vector)
+    similarity_scores = similarity_scores.flatten()
+    cumulative_similarity_score = np.sum(similarity_scores)
+    print("Similarity Scores:", similarity_scores)
+    print("Cumulative Similarity Score:", cumulative_similarity_score)
+
+
+df_user_vector = df_user.drop(columns=['id','name'])
+df_recs_vector = df_recs.drop(columns=['id','name'])
+print(df_recs_vector.dtypes, df_user_vector.dtypes)
+similarity_scoring(df_recs_vector.iloc[0].values.reshape(1, -1),df_user_vector)
