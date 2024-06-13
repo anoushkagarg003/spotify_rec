cbe1903b1b6e585a89736b44381723c9232fc51e
anoushkagarg003 <108232467+anoushkagarg003@users.noreply.github.com>
2024-06-13 12:07:15 +0530

    this version should work but is taking hours.

--- flaskverold.py
+++ flaskverold.py
@@ -3,6 +3,7 @@ from spotipy.oauth2 import SpotifyOAuth
 from flask import Flask, session, redirect, request, url_for, render_template
 from spotipy.cache_handler import FlaskSessionCacheHandler
 from sqlsample import get_connection, execute_query
+import mysql.connector
 import time
 connection=get_connection()
 
@@ -20,46 +21,66 @@ cache_handler = FlaskSessionCacheHandler(session)
 sp_oauth = SpotifyOAuth(scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, cache_handler=cache_handler, show_dialog=True)
 
 def create_tables():
-    with connection.cursor() as cursor:
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS user_top_songs_1 (
-                id VARCHAR(255) PRIMARY KEY,
-                name VARCHAR(255),
-                acousticness FLOAT,
-                danceability FLOAT,
-                duration_ms INT,
-                energy FLOAT,
-                instrumentalness FLOAT,
-                `key` INT,
-                liveness FLOAT,
-                loudness FLOAT,
-                mode INT,
-                speechiness FLOAT,
-                tempo FLOAT,
-                time_signature INT,
-                valence FLOAT
-            );
-        """)
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS potential_recommendations_2 (
-    id VARCHAR(255) PRIMARY KEY,
-    name VARCHAR(255),
-    acousticness FLOAT,
-    danceability FLOAT,
-    duration_ms INT,
-    energy FLOAT,
-    instrumentalness FLOAT,
-    key_value INT,
-    liveness FLOAT,
-    loudness FLOAT,
-    mode INT,
-    speechiness FLOAT,
-    tempo FLOAT,
-    time_signature INT,
-    valence FLOAT
-                       );
-        """)
-    connection.commit()
+    try:
+        with connection.cursor() as cursor:
+            # Drop and create user_top_songs_1 table
+            #cursor.execute("DROP TABLE IF EXISTS user_top_songs_1;")
+            
+            # Committing the drop operation
+            connection.commit()
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS user_top_songs_1 (
+                    id VARCHAR(255) PRIMARY KEY,
+                    name VARCHAR(255),
+                    acousticness FLOAT,
+                    danceability FLOAT,
+                    duration_ms INT,
+                    energy FLOAT,
+                    instrumentalness FLOAT,
+                    `key` INT,
+                    liveness FLOAT,
+                    loudness FLOAT,
+                    mode INT,
+                    speechiness FLOAT,
+                    tempo FLOAT,
+                    time_signature INT,
+                    valence FLOAT
+                );
+            """)
+            
+            # Sleep for 10 seconds (if this is necessary)
+            time.sleep(10)
+            #cursor.execute("DROP TABLE IF EXISTS potential_recommendations_2;")
+            
+            # Committing the drop operation
+            connection.commit()
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS potential_recommendations_2 (
+                    id VARCHAR(255) PRIMARY KEY,
+                    name VARCHAR(255),
+                    acousticness FLOAT,
+                    danceability FLOAT,
+                    duration_ms INT,
+                    energy FLOAT,
+                    instrumentalness FLOAT,
+                    key_value INT,
+                    liveness FLOAT,
+                    loudness FLOAT,
+                    mode INT,
+                    speechiness FLOAT,
+                    tempo FLOAT,
+                    time_signature INT,
+                    valence FLOAT
+                );
+            """)
+        
+        connection.commit()  # Committing changes to the database
+    
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        connection.rollback()  # Rolling back changes in case of error
+
+
 
 # Spotify authentication route
 @app.route('/')
@@ -115,27 +136,32 @@ def home():
     artists = {artist['id']: artist['name'] for artist in top_artists['items']}
 
     # Process each top artist to find similar artists and their top tracks
+    track_ids=[]
     for artist_id in artists.keys():
         similar_artists = sp.artist_related_artists(artist_id)
-        with connection.cursor() as cursor:
-            for artist in similar_artists['artists']:
-                similar_artist_id = artist['id']
+        for artist in similar_artists['artists']:
+            similar_artist_id = artist['id']
             # Fetch top tracks of each similar artist
-                top_tracks = sp.artist_top_tracks(similar_artist_id)
+            top_tracks = sp.artist_top_tracks(similar_artist_id)
                 # Prepare a list of track IDs for audio features retrieval
-                '''track_ids = [track['id'] for track in top_tracks['tracks']]
-                track_ids_names = []
+            for track in top_tracks['tracks']:
+                track_ids.append(track['id'])
+                '''track_ids_names = []
                 for track_id in track_ids:
                     track_info = sp.track(track_id)
                     track_name = track_info['name']
                     track_ids_names.append({'id':track_id, 'name':track_name})'''
                 # Chunk the track IDs into batches of 100 for audio features retrieval
-                audio_features = sp.audio_features(tracks=track_ids)
-                    # Insert potential recommendations into the database
-                '''for track, track_info in zip(audio_features, track_ids_names):'''
+    for i in range(0, len(track_ids), 100):
+        chunk = track_ids[i:i + 100]
+        audio_features1 = sp.audio_features(tracks=chunk)
+        audio_features.extend(audio_features1)
+                # Insert potential recommendations into the database
+                #for track, track_info in zip(audio_features, track_ids_names):
                 #time.sleep(10)
-                for track in audio_features:
-                    cursor.execute("""
+    with connection.cursor() as cursor:
+            for track in audio_features:
+                cursor.execute("""
                             INSERT INTO potential_recommendations_1 
                             (id, name, acousticness, danceability, duration_ms, energy, 
                              instrumentalness, key_value, liveness, loudness, mode, 
@@ -156,7 +182,7 @@ def home():
                             tempo = VALUES(tempo), 
                             time_signature = VALUES(time_signature), 
                             valence = VALUES(valence);
-                        """, (track['id'], 'null', track['acousticness'], 
+                        """, (track['id'], None , track['acousticness'], 
                               track['danceability'], track['duration_ms'], track['energy'], 
                               track['instrumentalness'], track['key'], track['liveness'], 
                               track['loudness'], track['mode'], track['speechiness'], 
